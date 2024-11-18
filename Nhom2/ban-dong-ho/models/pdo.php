6020b27565ab77e3d85d9bf21517745c55224fdd
TrongBang2436 <bangnvtph49788@gmail.com>
2024-11-19 05:19:04 +0700

    ket noi database

---
+++ Nhom2/ban-dong-ho/models/pdo.php
@@ -0,0 +1,20 @@
+<?php
+$host = '127.0.0.1';
+$db = 'duan1';
+$user = 'root';
+$pass = '';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Đã kết nối thành công!";
+} catch (PDOException $e) {
+    die("Kết nối thất bại: " . $e->getMessage());
+}
+?>
